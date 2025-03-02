c3c43ebbef72d9736b374cd16868c2c2b5e662d6
Anthony-Kishan <anthony.kishan01@gmail.com>
2025-03-02 14:58:10 +0600

    leadSeeder added to databse seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserModelSeeder::class,
             SliderModelSeeder::class,
+            LeadSeeder::class
         ]);
     }
 }
