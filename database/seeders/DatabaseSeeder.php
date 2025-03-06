e63ac69344df8f3ab5a8682f7374abaee7287764
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2025-03-06 12:38:31 +0600

    updated databse seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserModelSeeder::class,
             WorldSeeder::class,
-            // SliderModelSeeder::class,
-            // LeadSeeder::class
+            SliderModelSeeder::class,
+            LeadSeeder::class
         ]);
     }
 }
