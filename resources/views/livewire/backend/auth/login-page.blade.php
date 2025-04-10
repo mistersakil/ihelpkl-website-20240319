76f19041bb86dc115bd2cf2b5a07c01b71914fb6
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2025-04-10 17:41:33 +0600

    admin login page static text langulal support done

--- resources/views/livewire/backend/auth/login-page.blade.php
+++ resources/views/livewire/backend/auth/login-page.blade.php
@@ -24,11 +24,11 @@
                             {{-- <img src="{{ Vite::imageRoot('logo.svg') }}" width="160" alt="logo" /> --}}
                         </div>
                         <div class="text-center mb-4">
-                            <h5 class="">
+                            <h4>
                                 {{ __('login as admin') }}
-                            </h5>
+                            </h4>
                             <p class="mb-0">
-                                {{ __('please fill the below details to login') }}
+                                {{ __('please provide the details below to log in') }}
                             </p>
                         </div>
                         <div class="form-body">
@@ -36,7 +36,7 @@
                                 wire:submit.prevent="login_process">
                                 <div class="col-12">
                                     <label for="email" class="form-label">
-                                        {{ __('Email Address') }}
+                                        {{ __('email address') }}
                                     </label>
 
                                     <div class="input-group">
@@ -45,7 +45,7 @@
                                         </div>
                                         <input wire:model.lazy="email" type="text"
                                             class="form-control @error('email') is-invalid @enderror" id="email"
-                                            placeholder="{{ __('Enter your email address') }}">
+                                            placeholder="{{ __('enter your email address') }}">
 
                                         @error('email')
                                             <div class="invalid-feedback">
@@ -59,10 +59,10 @@
 
                                 <div class="col-12">
                                     <label for="password" class="form-label">
-                                        {{ __('Enter Password') }}
+                                        {{ __('enter password') }}
                                     </label>
                                     <div class="input-group" id="show_hide_password">
-                                        <div class="input-group-text toggle" title="Show or hide password">
+                                        <div class="input-group-text toggle" title="{{ __('show or hide password') }}">
                                             <i class="{{ _icons('hide') }}"></i>
                                         </div>
                                         <input wire:model.lazy="password" type="password"
@@ -84,7 +84,7 @@
                                         <input wire:model="remember_me" class="form-check-input" type="checkbox"
                                             id="remember_me" @if ($remember_me) checked @endif />
                                         <label class="form-check-label"
-                                            for="remember_me">{{ __('Remember Me') }}</label>
+                                            for="remember_me">{{ __('remember me') }}</label>
                                     </div>
                                 </div>
                                 <!-- /.col -->
@@ -93,7 +93,7 @@
                                     <div class="d-grid">
                                         <button type="submit" class="btn btn-primary">
                                             <i class="bx bxs-lock-open"></i>
-                                            {{ __('Login') }}
+                                            {{ __('login') }}
                                         </button>
                                     </div>
                                 </div>
