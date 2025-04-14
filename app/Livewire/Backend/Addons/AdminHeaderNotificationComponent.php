06502489b9f74724276e5123755d2691bd50084f
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2025-04-14 17:24:52 +0600

    visitors query notification section design updated

--- app/Livewire/Backend/Addons/AdminHeaderNotificationComponent.php
+++ app/Livewire/Backend/Addons/AdminHeaderNotificationComponent.php
@@ -2,11 +2,23 @@
 
 namespace App\Livewire\Backend\Addons;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
+/**
+ * @author Sakil Jomadder <sakil.diu.cse@gmail.com>
+ */
 class AdminHeaderNotificationComponent extends Component
 {
-    public function render()
+    ## Component props
+    public int $unreadQueryCount = 0;
+
+    /**
+     * Render view
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function render(): View
     {
         return view('livewire.backend.addons.admin-header-notification-component');
     }
--- resources/views/livewire/backend/addons/admin-header-notification-component.blade.php
+++ resources/views/livewire/backend/addons/admin-header-notification-component.blade.php
@@ -8,38 +8,34 @@
         style="display: none; position: absolute; top: 102%; right: 0%;">
         <a href="javascript:;">
             <div class="msg-header">
-                <p class="msg-header-title">My Cart</p>
-                <p class="msg-header-badge">10 Items</p>
+                <p class="msg-header-title">{{ __('visitors query') }}</p>
+                <p class="msg-header-badge">{{ $unreadQueryCount }} {{ __('items') }}</p>
             </div>
         </a>
         <div class="header-message-list">
             <a class="dropdown-item" href="javascript:;">
                 <div class="d-flex align-items-center gap-3">
                     <div class="position-relative">
-                        <div class="cart-product rounded-circle bg-light">
-                            <i class="{{ _icons('bell') }}"></i>
-                        </div>
+                        {{-- <div class="cart-product rounded-circle bg-light"> --}}
+                        <i class="rounded-circle bg-info p-1 {{ _icons('bell') }}"></i>
+                        {{-- </div> --}}
                     </div>
                     <div class="flex-grow-1">
                         <h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
                         <p class="cart-product-price mb-0">1 X $29.00</p>
                     </div>
-                    <div class="">
-                        <p class="cart-price mb-0">$250</p>
+
+                    <div class="form-check form-check-warning">
+                        <input class="form-check-input" type="radio" checked />
                     </div>
-                    <div class="cart-product-cancel"><i class="bx bx-x"></i>
-                    </div>
+
                 </div>
             </a>
 
         </div>
         <a href="javascript:;">
             <div class="text-center msg-footer">
-                <div class="d-flex align-items-center justify-content-between mb-3">
-                    <h5 class="mb-0">Total</h5>
-                    <h5 class="mb-0 ms-auto">$489.00</h5>
-                </div>
-                <button class="btn btn-primary w-100">Checkout</button>
+                <button class="btn btn-primary w-100">{{ __('view all') }}</button>
             </div>
         </a>
     </div>
